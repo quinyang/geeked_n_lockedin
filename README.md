9e9bc4232566707b8678da1b76da6771a3f6df9d
Jiakun Yang <jiakuny1@uci.edu>
2025-02-04 16:52:18 -0800

    Merge remote changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # geeked_n_lockedin
-Geeked vs Lockedin 
 This is the local version.
 This is the remote version with additional details.
+
---
+++ android/app/src/main/kotlin/com/example/geeked_n_lockedin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.geeked_n_lockedin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
